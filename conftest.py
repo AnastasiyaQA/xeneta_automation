caab6be4b8ff0d6f7962ea397a71226d33ba8669
AnastasiyaQA <77677607+AnastasiyaQA@users.noreply.github.com>
2021-02-15 10:20:56 +0300

    chromedriver jenkins aws

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 @fixture(scope='function')
 def chrome_browser():
-    browser = webdriver.Chrome()
+    driver = webdriver.Chrome('/usr/bin/chromedriver')
     browser.maximize_window()
     return browser
 
