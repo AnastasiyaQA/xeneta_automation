b8ad3e1d72b54f8401de4a0d1f62ff3a5970058c
AnastasiyaQA <77677607+AnastasiyaQA@users.noreply.github.com>
2021-02-15 15:31:39 +0300

    undo

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 @fixture(scope='function')
 def chrome_browser():
-    browser = webdriver.Chrome('/usr/bin')
+    browser = webdriver.Chrome()
     browser.maximize_window()
     return browser
 
